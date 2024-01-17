afbc82b644f09033a248980e4b80b34bdd8cbb0c
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-17 10:59:31 +0100

    [edit] navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Button from '@/components/button';
@@ -17,12 +18,12 @@ export default function Home() {
   };
 
   function handleJoin(e: any) {
-    toast('You need to be initiated, Folk');
+    toast('You need to be chad! Try later');
   }
   return (
     <>
-      <main className='flex flex-col h-[90vh] items-center justify-center gap-3 md:gap-5'>
-        <h1 className='font-bold text-base md:text-4xl'>
+      <main className='flex flex-col h-[90vh] mx-3 md:mx-[100px] items-center justify-center gap-3 md:gap-5'>
+        <h1 className='font-bold text-2xl md:text-4xl'>
           where chad men live and strives!!!
         </h1>
         <form onSubmit={handleSubmit}>
@@ -31,7 +32,7 @@ export default function Home() {
             <Button
               type='button'
               onClick={handleJoin}
-              className='bg-black active:bg-black/90 rounded-md w-[100px] md:w-[200px] text-center text-base font-medium p-2 md:p-4 text-white transition-all duration-300 active:scale-95'
+              className='bg-black active:bg-black/90 rounded-md w-[100px] lg:w-[200px] text-center text-base font-medium p-2 md:p-4 text-white transition-all duration-300 active:scale-95'
               loading={loading}
             >
               Join em?
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,10 +3,7 @@ export default function Navbar() {
     <div>
       <header className='mx-3 mt-5 md:mx-10'>
         <nav className='flex flex-row items-center'>
-          <h1 className='text-2xl font-bold'>
-            Freemen
-            <span>💀</span>
-          </h1>
+          <h1 className='text-2xl font-bold'>{`<Freemen!👩‍💻/>`}</h1>
         </nav>
       </header>
     </div>
